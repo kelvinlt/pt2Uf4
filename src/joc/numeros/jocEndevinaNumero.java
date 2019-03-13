e5a115da50c0c98fe48e89fc79d3587a41bea531
x2382383c <x2382383c@killer-10.infomerce.es>
2019-03-13 18:57:48 +0100

    modificaciones endevinanumero

--- src/joc/numeros/jocEndevinaNumero.java
+++ src/joc/numeros/jocEndevinaNumero.java
@@ -10,11 +10,25 @@ package joc.numeros;
  * @author x2382383c
  */
 public class jocEndevinaNumero extends joc.joc{
+    int randomNum;
     
     public jocEndevinaNumero() {
         super(0, 0, 0);
     }
+    
+    public jocEndevinaNumero(int randomNum, int vidasRestantes, int vidasInicial, int record) {
+        super(vidasRestantes, vidasInicial, record);
+        this.randomNum = randomNum;
+    }
 
+    public int getRandomNum() {
+        return randomNum;
+    }
+
+    public void setRandomNum(int randomNum) {
+        this.randomNum = randomNum;
+    }
+     
     @Override
     public void juga() {
         reiniciaPartida();
